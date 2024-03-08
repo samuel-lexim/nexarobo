d60dae6d0ecffbbb0d98b50ccbceb8979661dcaa
hiep <hiep.dao@leximit.com>
2024-03-08 18:36:16 +0700

    footer

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -6,4 +6,11 @@ if (! defined('WP_DEBUG')) {
 
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+    // custom css
+    wp_enqueue_style('main-css', get_stylesheet_directory_uri() .'/css/main-style.css');
 });
+
+
+/* Disable WordPress Admin Bar for all users */
+add_filter('show_admin_bar', '__return_false');
