37b0a6642dd7c6ec8eac3b7cd1de8490837c23a9
hiep <hiep.dao@leximit.com>
2024-03-20 21:52:30 +0700

    PDP - our customer

--- wp-content/themes/blocksy-child/js/pdp.js
+++ wp-content/themes/blocksy-child/js/pdp.js
@@ -4,6 +4,7 @@ $(document).ready(function (){
     let PDP = {
         init: function () {
             this.icon_list_init();
+            this.our_customer_init();
         },
 
         icon_list_init: function () {
@@ -12,7 +13,44 @@ $(document).ready(function (){
             console.log(list.length);
             let number = (list.length % 2 === 0) ? "even" : "odd";
             container.addClass(number);
+        },
+
+        our_customer_init: function () {
+            let OC_slider = $('.pdp-testimonial_slider');
+            let items = $('.pdp-testimonial_slider > div');
+            let n = items.length;
+
+            if (OC_slider && OC_slider.length > 0) {
+                OC_slider.slick({
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    arrows: false,
+                    autoplay: false,
+                    infinite: false,
+                    dots: n > 3,
+                    centerPadding: '50px',
+                    responsive: [
+                        {
+                            breakpoint: 1200,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2,
+                                dots: n > 2,
+                            }
+                        },
+                        {
+                            breakpoint: 690,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                dots: n > 1,
+                            }
+                        }
+                    ]
+                });
+            }
         }
+
     };
 
     PDP.init();
