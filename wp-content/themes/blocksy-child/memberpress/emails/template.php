f1643da9baccfde575a45b1117d5493d7452ac24
hiep <hiep.dao@leximit.com>
2024-06-10 18:00:26 +0700

    Email template - Welcome from register plugin

---
+++ wp-content/themes/blocksy-child/memberpress/emails/template.php
@@ -0,0 +1,37 @@
+<?php if (!defined('ABSPATH')) {
+    die('You are not allowed to call this page directly.');
+} ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;UTF-8">
+    <style type="text/css">
+        @font-face {
+            font-family: 'ProximaNova';
+            font-weight: 400;
+            src: url('https://nexarobo.wpenginepowered.com/wp-content/uploads/2024/03/ProximaNova-Regular.woff2') format('woff2');
+        }
+
+        @font-face {
+            font-family: 'ProximaNova';
+            font-weight: 600;
+            src: url('https://nexarobo.wpenginepowered.com/wp-content/uploads/2024/03/ProximaNova-Semibold.woff2') format('woff2');
+        }
+
+        body {
+            font-family: "ProximaNova", sans-serif;
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 16px;
+            background-color: #fff;
+            padding: 0;
+            color: #252525;
+        }
+    </style>
+</head>
+<body style="font-family: 'ProximaNova', sans-serif; padding: 0; font-weight: 400;
+   font-size: 14px;line-height: 16px;background-color: #fff;color: #252525;">
+<?php echo $body; ?>
+</body>
+</html>
+
